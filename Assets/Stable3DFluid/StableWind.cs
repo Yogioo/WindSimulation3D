7ce994f8760f811097b59d4fcb4356ecb887c199
Masaka_Yogi <1273750265@qq.com>
2021-12-23 16:42:34 +0800

    Debug

--- Assets/Stable3DFluid/StableWind.cs
+++ Assets/Stable3DFluid/StableWind.cs
@@ -6,20 +6,6 @@ using UnityEngine.Rendering;
 
 public class StableWind : MonoBehaviour
 {
-    public ComputeShader Compute;
-
-    public bool Advect = true;
-    public bool Diffusion = true;
-    public bool Force = true;
-
-    [Header("平流系数")]
-    public float AdvectValue = 1;
-
-    [Header("扩散系数")]
-    public float _V;
-    [Header("扩散迭代次数"), Range(0, 25)]
-    public int _Iteration = 20;
-
     static class Kernels
     {
         public const int Advect = 0;
@@ -28,18 +14,45 @@ public class StableWind : MonoBehaviour
         public const int Gradient = 3;
         public const int AddForce = 4;
         public const int Clear = 5;
-    }
+        public const int Clear3D = 6;
+        public const int JacobiPressure = 7;
 
+    }
     static class VFB
     {
         public static RenderTexture V1;
         public static RenderTexture V2;
         public static RenderTexture V3;
+        public static RenderTexture DV;
         public static RenderTexture P1;
         public static RenderTexture P2;
     }
 
+    public ComputeShader Compute;
+
+    public bool Advect = true;
+    public bool Diffusion = true;
+    public bool Force = true;
+    public bool Divergence = true;
+    public bool Pressure = true;
+
+
+    [Header("平流系数")]
+    public float AdvectValue = 1;
+
+    [Header("扩散系数")]
+    public float _V;
+    [Header("扩散迭代次数"), Range(10, 25)]
+    public int _Iteration = 20;
+
+    [Header("压力迭代次数"), Range(20, 40)]
+    public int _IterationPressure = 20;
+
+    public float PressureValue = 0.9f;
+
+
     public Vector3Int Size = new Vector3Int(32, 16, 32);
+    public Vector3 _WindCenter, _DivisionSize;
 
     int ThreadCountX { get { return Size.x / 8; } }
     int ThreadCountY { get { return Size.y / 8; } }
@@ -48,6 +61,8 @@ public class StableWind : MonoBehaviour
     int ResolutionY { get { return ThreadCountY * 8; } }
     int ResolutionZ { get { return ThreadCountZ * 8; } }
 
+    public bool DisplayDivergence = false;
+    public bool DisplayPressure = false;
     RenderTexture AllocateBuffer(int componentCount, int width = 0, int height = 0)
     {
         var format = RenderTextureFormat.ARGBHalf;
@@ -60,11 +75,10 @@ public class StableWind : MonoBehaviour
 
 
         var rt = new RenderTexture(width, height, 0, format);
-        if (componentCount == 3)
-        {
-            rt.dimension = TextureDimension.Tex3D;
-            rt.volumeDepth = ResolutionZ;
-        }
+        //3D Texture
+        rt.dimension = TextureDimension.Tex3D;
+        rt.volumeDepth = ResolutionZ;
+
         rt.enableRandomWrite = true;
         rt.Create();
         return rt;
@@ -75,6 +89,7 @@ public class StableWind : MonoBehaviour
         VFB.V1 = AllocateBuffer(3);
         VFB.V2 = AllocateBuffer(3);
         VFB.V3 = AllocateBuffer(3);
+        VFB.DV = AllocateBuffer(1);
         VFB.P1 = AllocateBuffer(1);
         VFB.P2 = AllocateBuffer(1);
     }
@@ -83,6 +98,7 @@ public class StableWind : MonoBehaviour
         Destroy(VFB.V1);
         Destroy(VFB.V2);
         Destroy(VFB.V3);
+        Destroy(VFB.DV);
         Destroy(VFB.P1);
         Destroy(VFB.P2);
     }
@@ -142,28 +158,63 @@ public class StableWind : MonoBehaviour
             Compute.Dispatch(Kernels.AddForce, ThreadCountX, ThreadCountY, ThreadCountZ);
         }
 
-        //// Divergence
-        //Compute.SetTexture(Kernels.Clear, "U_out", VFB.V1);
-        //Compute.Dispatch(Kernels.Clear, ThreadCountX, ThreadCountY, ThreadCountZ);
-        //Compute.SetTexture(Kernels.Divergence, "U_out", VFB.V1);
+        // Divergence
+
+        // Divergence Init
+        if (Divergence)
+        {
+            Compute.SetTexture(Kernels.Divergence, "U_in", VFB.V1);
+            Compute.SetTexture(Kernels.Divergence, "Dv_out", VFB.DV);
+            Compute.Dispatch(Kernels.Divergence, ThreadCountX, ThreadCountY, ThreadCountZ);
+        }
+
+        if (Pressure)
+        {
+            // Clear Pressure
+            Compute.SetTexture(Kernels.Clear3D, "P_out", VFB.P1);
+            Compute.Dispatch(Kernels.Clear3D, ThreadCountX, ThreadCountY, ThreadCountZ);
+            // Calc Gradient
+            Compute.SetTexture(Kernels.JacobiPressure, "Dv_in", VFB.DV);
+            alpha = - 1 * PressureValue;
+            beta = 1 / (6.0f);
+            Compute.SetFloat("Alpha", alpha);
+            Compute.SetFloat("Beta", beta);
+            for (int i = 0; i < _IterationPressure; i++)
+            {
+                Compute.SetTexture(Kernels.JacobiPressure, "P_in", VFB.P1);
+                Compute.SetTexture(Kernels.JacobiPressure, "P_out", VFB.P2);
+                Compute.Dispatch(Kernels.JacobiPressure, ThreadCountX, ThreadCountY, ThreadCountZ);
 
 
-        Shader.SetGlobalTexture("_VelocityMap", VFB.V1);
+                Compute.SetTexture(Kernels.JacobiPressure, "P_in", VFB.P2);
+                Compute.SetTexture(Kernels.JacobiPressure, "P_out", VFB.P1);
+                Compute.Dispatch(Kernels.JacobiPressure, ThreadCountX, ThreadCountY, ThreadCountZ);
+            }
+            // Minus Gradient
+            Compute.SetTexture(Kernels.Gradient, "P_in", VFB.P1);
+            Compute.SetTexture(Kernels.Gradient, "U_out", VFB.V1);
+            Compute.Dispatch(Kernels.Gradient, ThreadCountX, ThreadCountY, ThreadCountZ);
+        }
+
+
+
+        if (DisplayDivergence)
+        {
+            Shader.SetGlobalTexture("_VelocityMap", VFB.DV);
+        }
+        else if (DisplayPressure)
+        {
+            Shader.SetGlobalTexture("_VelocityMap", VFB.P1);
+        }
+        else
+        {
+            Shader.SetGlobalTexture("_VelocityMap", VFB.V1);
+        }
+
+
+
+
         Shader.SetGlobalVector("_WindCenter", _WindCenter);
         Shader.SetGlobalVector("_DivisionSize", _DivisionSize);
     }
-
-    
-    [MenuItem("清除")]
-    public void Clear()
-    {
-        Compute.SetTexture(Kernels.Clear, "U_out", VFB.V1);
-        Compute.Dispatch(Kernels.Clear, ThreadCountX, ThreadCountY, ThreadCountZ);
-    }
-
-    public Vector3 _WindCenter, _DivisionSize;
-
-
-
-
 }
